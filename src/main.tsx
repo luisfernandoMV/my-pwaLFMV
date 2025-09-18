802da65e22b73438628e3565fb2f7adf24127b54
luisfernandoMV <luisfernandomateovargas894@gmail.com>
2025-09-17 22:25:23 -0600

    Version estable con el paso numero 8

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App />,
   </StrictMode>,
+  
 )
+// 🔹 Registrar el Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then((registration) => {
+        console.log('✅ Service Worker registrado con éxito:', registration)
+      })
+      .catch((error) => {
+        console.error('❌ Error al registrar el Service Worker:', error)
+      })
+  })
+}
